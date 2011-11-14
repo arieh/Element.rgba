59d391702e0407729652fd59217a16ae5d5158a1
Arieh Glazer <arieh.glazer@gmail.com>
2011-11-14 17:01:51 +0200

    fixed a few bugs

--- Source/Element.setRGBA.js
+++ Source/Element.setRGBA.js
@@ -16,23 +16,22 @@ provides: [Element.setRGBA,Browser.Features.rgba]
 */
 (function(){
     var re = /^rgba/,
-            supported = null;
-
-    if(has("css-enabled")){
+            supported = null,
+			el = document.createElement('div');
+			
         try{
               el.style.color = "rgba(1,1,1,0.5)";
               supported = re.test(el.style.color);
               el.style.color = "";
         }catch(e){}
-    }
         
     Browser.Features.rgba = supported;
     
-     Element.implement({
+    Element.implement({
         setRGBA : Browser.Features.rgba ?
             function setRGBA(r,g,b,a){
                 this.setStyle('background-color','rgba('+arguments.join(',')+')');
-                return this;`
+                return this;
             }  
             : function setRGBA(r,g,b,a){
                 var rgba = Array.from(arguments);
@@ -50,7 +49,7 @@ provides: [Element.setRGBA,Browser.Features.rgba]
                             
                 this.style.backgroundColor = "transparent";
                 this.style.zoom = 1;                        
-                this.style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr=#"+match.join("")+",endColorstr=#"+match.join("")+");"
+                this.style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr=#"+rgba.join("")+",endColorstr=#"+rgba.join("")+")"
                 
                 return this;
             }    
